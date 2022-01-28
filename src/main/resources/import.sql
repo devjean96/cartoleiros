f191972ab6f2586431662c9de561b448ff9f3112
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-01-28 18:44:25 -0300

    atualizacao script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,10 @@
 INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'BAHEACELONA BA', 'JEANP');
+INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'BAHIASHOW7', 'INHA');
+INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'CLUBE RIACHO FUNDO', 'FINHO');
+INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'RB RIBEIRO 13', 'LUAN ZE');
+INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'BRANDAO TEAN', 'ROBSON');
+INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'POZAR F.C', 'WELL');
+
+INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('MUNDIAL', 'SEM TROFEU', 1);
+INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('COPA', 'SEM TROFEU', 3);
+INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('LIGA', 'SEM TROFEU', 2);
