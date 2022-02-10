63a276f6892b7e9d0361e118c151b7719bf37da0
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-02-10 18:20:00 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,14 @@ INSERT INTO tb_equipe(escudo, nome, treinador) VALUES ('SEM ESCUDO', 'POZAR F.C'
 INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('MUNDIAL', 'SEM TROFEU', 1);
 INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('COPA', 'SEM TROFEU', 3);
 INSERT INTO tb_competicao(nome, trofeu, peso) VALUES ('LIGA', 'SEM TROFEU', 2);
+
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (1, 2, 22);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (2, 1, 3);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (2, 2, 17);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (3, 1, 1);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (3, 2, 22);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (4, 1, 1);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (4, 2, 23);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (5, 2, 19);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (5, 3, 1);
+INSERT INTO tb_pontuacao(equipe_id, competicao_id, trofeus) VALUES (6, 2, 17);
